d2c3a46a06b1f7171ffd27f07a7555b7040c4da5
atomtables <76572470+atomtables@users.noreply.github.com>
2024-06-08 14:31:05 -0400

    added a hero

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,9 @@
 <div class="navbar bg-primary text-primary-content">
   <div class="navbar-start">
     <div class="dropdown">
-      <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+      <div tabindex="-1" role="button" class="btn btn-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7"/>
+        </svg>
       </div>
       <ul tabindex="-1" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52 text-secondary-content bg-secondary">
         <li><a href="#a">Homepage</a></li>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="hero min-h-[calc(100vh-64px)]" style="background-image: url(/frontpageimage.png);">
+  <div class="hero-overlay bg-opacity-60"></div>
+  <div class="hero-content text-center text-neutral-content">
+    <div class="max-w-lg">
+      <h1 class="mb-5 text-5xl font-bold">A Stock Predictor: Developed for the Safe</h1>
+      <p class="mb-5">Using data from popular stock data sources, media articles, and other sources, we have created a stock predictor that predicts, on a monthly basis, whether you should buy into a stock. We made this for the unwashed masses who go through student debt. </p>
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
